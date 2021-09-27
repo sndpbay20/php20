96e5db789e0e9f751290a9b7cfc9b019445c7d09
sndpbay20 <88138178+sndpbay20@users.noreply.github.com>
2021-09-27 19:02:11 +0545

    Add files via upload

---
+++ 03_good/views/products/create.php
@@ -0,0 +1,42 @@
+<h1>Create new Product</h1>
+
+
+
+
+<?php if (!empty($errors)) : ?>
+    <div class="alert alert-danger">
+      <?php foreach ($errors as $error) :  ?>
+        <div><?php echo $error ?></div>
+      <?php endforeach ?>
+    </div>
+
+  <?php endif;  ?>
+
+
+  <form action="" method="post" enctype="multipart/form-data">
+
+  <?php if ($Product['image']): ?>
+          <img src="/<?php echo $product['image'] ?>" class="update-image">
+          <?php endif; ?>
+
+
+    <div class="form-group">
+      <label>Product Image</label>
+      <br>
+      <input type="file" name="image">
+    </div>
+    <div class="form-group">
+      <label>Product Title</label>
+      <input type="text" name="title" class="form-control" value="<?php echo $Product['title'] ?>">
+    </div>
+    <div class="form-group">
+      <label>Product Description</label>
+      <textarea class="form-control" name="description"><?php echo $Product['description'] ?></textarea>
+    </div>
+    <div class="form-group">
+      <label>Product Price</label>
+      <input type="number" step=".01" name="price" value="<?php echo $product['price'] ?>" class="form-control">
+    </div><br>
+
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
